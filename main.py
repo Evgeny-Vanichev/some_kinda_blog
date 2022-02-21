3b234ab787325cc83e5817d87390dd92dab0b7be
evgeny vanichev <e.vanitschev@yandex.ru>
2022-02-21 16:59:41 +0300

    ДОбавление формы регистрации

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 from flask import Flask, render_template, redirect
+from flask_login import login_user, LoginManager
 
 from data import db_session
 from data.news import News
+from forms.LoginForm import LoginForm
 from forms.user import RegisterForm
 from data.news import News
 from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
